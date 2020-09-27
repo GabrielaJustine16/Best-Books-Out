862acdb4c0c5c168dca9bc82f7e8c491d3c25ec3
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-09-27 15:26:38 -0700

    adds has_many relationships to the models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :author
-  belongs_to :user
+  belongs_to :user #creator of it
+  has_many :reviews
+  has_many :users, through: :reviews #ppl who have reviewed it
 end
