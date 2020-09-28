0b855b7bb2ffcc995f420ca7643a1e815366c364
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-09-28 15:09:00 -0700

    adds show method to users controller and fixes the user params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :reviewed_books, through: :reviews, source :book
+    has_many :reviewed_books, through: :reviews, source: :book
 
     has_many :books #they have created
+
+    validates :username, uniqueness: true, presence: true
+
+    has_secure_password
 end
