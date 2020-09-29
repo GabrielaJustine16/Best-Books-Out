18ad7c81e38858093f1bcc75a8e8d76f97ce38f3
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-09-28 18:46:50 -0700

    adds create action to the books controller and the params method for book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,24 @@
 class BooksController < ApplicationController
+    
     def new 
-        @book=Books.new
+        @book=Book.new
+        @book.build_author
     end 
+
+    def create 
+        @book = Book.new(book_params)
+        @book.user_id = session[:user_id]
+    if @book.save #causes errir but says why !
+            redirect_to book_path(@book)
+        else 
+            render :new
+        end
+    end 
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title,:description,:author_id,author_attributes: [:name])
+
+    end    
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,15 +3,15 @@ class Book < ApplicationRecord
   belongs_to :user #creator of it
   has_many :reviews
   has_many :users, through: :reviews #ppl who have reviewed it
+  accepts_nested_attributes_for :author
 
+  #def author
+  #end 
 
-  def author
-  end 
+  #def author=
+  #end 
 
-  def author=
-  end 
-
-  def author_id
+  #def author_id
 
 
 
