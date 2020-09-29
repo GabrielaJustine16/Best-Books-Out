028143c1ea790a79567444d3e31fe72bab3e7903
Gabriela Johnson <johnsongabriela12@gmail.com>
2020-09-29 10:27:18 -0700

    changes the field for star ratings to number field instead

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,7 @@ class ReviewsController < ApplicationController
         #now a nested route so change id to book_id
         @book = Book.find_by_id(params[:book_id])
         #review object
-        @review = @book.build_review
+        @review = @book.reviews.build
         #so it knows what to associate with and bc its a belomngs ro
 
         #review belongs to an ice cream
@@ -12,4 +12,6 @@ class ReviewsController < ApplicationController
 
     def index
     end 
+
+    
 end
